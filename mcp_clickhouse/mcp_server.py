95494beac21b9b1b5e897e7f2564e53af400d8ad
barifrah27 <bar@docontrol.io>
2025-11-26 10:00:18 +0200

    SAVEPOINT

--- mcp_clickhouse/mcp_server.py
+++ mcp_clickhouse/mcp_server.py
@@ -464,7 +464,10 @@ def execute_query(query: str, ctx: Context):
 def run_select_query(query: str, ctx: Context):
     """Run a SELECT query in a ClickHouse database"""
     logger.info(f"Executing SELECT query: {query}")
-    logger.info(f"context of the run selelt query", extra={"ctx": ctx})
+    logger.info(
+        f"context of the run selelt query {str(ctx.request_context.meta)}",
+        extra={"ctx": ctx},
+    )
     try:
         future = QUERY_EXECUTOR.submit(execute_query, query, ctx)
         try:
